43ce241b7dea541e638ad22c99cde9d3b446046c
s0nprem0 <s0nprem0@proton.me>
2026-05-19 01:13:32 +0800

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 
 Route::get('/', [ChirpController::class, 'index']);
 Route::post('/chirps', [ChirpController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+Route::resource('chirps', ChirpController::class)
+	->only(['index', 'store', 'edit', 'update', 'destroy']);
